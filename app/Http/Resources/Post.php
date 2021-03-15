46666640a4e09a714adfa921868b3d999e7527e2
berking-da-quan <ishimweeschylebersy@gmail.com>
2021-03-15 17:00:38 +0200

    done the resource files

--- app/Http/Resources/Post.php
+++ app/Http/Resources/Post.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\User;
+use Carbon\Carbon;
 
 class Post extends JsonResource
 {
@@ -16,14 +17,15 @@ class Post extends JsonResource
     public function toArray($request)
     {
         $username = User::find($this->id)->name;
-
+        $time = new Carbon($this->created_at);
         return [
             'title' => $this->title,
             'username' => $username,
             'image_url' => $this->image_url,
             'likes' => $this->likes + 5,
             'content' => $this->content,
-            'created_at' => $this->created_at,
+            'created_at' => $time->diffInHours('America/Toronto'),
+            // 'created_at' => date_format($this->created_at, 'Y-m-d'),
             'updated_at' => $this->updated_at
         ];
     }
